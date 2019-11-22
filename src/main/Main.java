98aba2aaa52fe10af088c03366271fea6d81a688
dim3662 <dim3662@gmail.com>
2019-11-23 00:20:49 +0300

    qwe
    qwe

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 // Подключение графических библиотек
+
+
+import com.itextpdf.*;
+
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.styledxmlparser.jsoup.nodes.Document;
 import design.Design;
 import managerGroup.ManagerGroup;
+
+
+import java.io.FileOutputStream;
 
 public class Main {
     public static void main(String[] args) {
